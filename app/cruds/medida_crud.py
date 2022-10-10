832684e4da7aafc41081b7829a7ca0dffc2a3652
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-10-10 15:07:08 -0500

    Se modifica getById y nuevo api de medida

--- app/cruds/medida_crud.py
+++ app/cruds/medida_crud.py
@@ -57,3 +57,26 @@ async def get_a_by_sub_product_type(subProductType:str):
             print('conexion terminada')
     return dict_json
 
+
+@router.get("/{idProductType}")
+async def get_by_product_type(idProductType:str):
+    dict_json = []
+    try:
+        conn = utils.conexion_postgres(host,port,db,usr,pwd)
+        
+        query = f"select row_to_json(row) from (select id, descripcion from medida where id_tipo_producto = '{idProductType}' order by descripcion ) row"
+        cursor = conn.cursor()
+        cursor.execute(query)
+        print('Query ejecutado')
+        if cursor.rowcount > 0:
+            records = cursor.fetchall()
+            for row in records:
+                dict_json.append(row[0])
+    except BaseException as error:
+        print(f'Ocurrió un error inesperado: {error.__str__}')
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+            print('conexion terminada')
+    return dict_json
